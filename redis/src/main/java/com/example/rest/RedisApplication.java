ef0c990a054dfefd9ce85bb3d3ee12b97dccbe64
narif1237 <narif1237@gmail.com>
2018-11-10 10:44:47 +0530

    just checking

--- redis/src/main/java/com/example/rest/RedisApplication.java
+++ redis/src/main/java/com/example/rest/RedisApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisApplication {
 
 	@Bean
-	edisConnectionFactory jedisConnectionFactory() {
+	disConnectionFactory jedisConnectionFactory() {
 		return new JedisConnectionFactory();
 	}
 
